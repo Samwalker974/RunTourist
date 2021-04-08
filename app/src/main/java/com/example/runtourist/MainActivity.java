0bdda4c0287a1219d77028900b941f3d22ad6b13
Samwalker974 <zerzamo@gmail.com>
2021-04-08 16:27:41 +0100

    Mise a jour: -Allignement des different Image et ajustement taille -Button start permettant ainsi d'afficher les img -Mise en place OnClick showImg:   -Lors du click sur Start affiche les Img   -Met le le Button start Invisible

--- app/src/main/java/com/example/runtourist/MainActivity.java
+++ app/src/main/java/com/example/runtourist/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.Button;
 import android.view.View;
 
 public class MainActivity extends Activity {
-private ImageView imageView, imageView2, imageView3, imageView4;
-private Button button1;
+    private ImageView imgN, imgE, imgO, imgS;
+    private Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,36 +18,23 @@ private Button button1;
         setContentView(R.layout.activity_main);
         
          // Constructeur qui recupere les id des imageView
-        this.imageView = (ImageView) this.findViewById(R.id.imageView);
-        this.imageView2 = (ImageView) this.findViewById(R.id.imageView);
-        this.imageView3 = (ImageView) this.findViewById(R.id.imageView3);
-        this.imageView4 = (ImageView) this.findViewById(R.id.imageView4);
+        // Modification des noms plus simple et identifiable
+        this.imgN = (ImageView) this.findViewById(R.id.imgN);
+        this.imgE = (ImageView) this.findViewById(R.id.imgE);
+        this.imgO = (ImageView) this.findViewById(R.id.imgO);
+        this.imgS = (ImageView) this.findViewById(R.id.imgS);
         this.button1 = (Button) this.findViewById(R.id.button1);
         
-        this.button1.setOnClickListener(new Button.OnClickListener(){
-
-            @Override
-            // Fonction a l'appui du bouton Start qui affiche une image
-            public void onClick(View v) {
-                showImage1();
-                //showImage2();
-
-            }
-
-
-        });
-    }
-    
-    // Mise en place l'image dans l'image view
-    private void showImage1() {
-        this.imageView.setImageResource(R.drawable.nord);
-
-       // this.imageView.setImageResource(R.drawable.ouest);
-      //  this.imageView.setImageResource(R.drawable.est);
-    }
-    private void showImage2(){
-        this.imageView.setImageResource(R.drawable.sud);
 
     }
+    // Fonction qui est appelle automatiquement lors de l'appuie du bouton start
+    public void showImg(View view){
+        imgN.setImageResource(R.drawable.nord);
+        imgE.setImageResource(R.drawable.est);
+        imgO.setImageResource(R.drawable.ouest);
+        imgS.setImageResource(R.drawable.sud);
+        button1.setVisibility(View.INVISIBLE);
+    }
+
 
 }
