09a8655abe6b9767621dcc5edb1e65b28c1c6827
MysterX02 <xavierl97490@hotmail.fr>
2021-04-26 08:42:15 +0400

    Base de donnée assez complet faudra faire des test mais imcomplet essaie d'implementation d'une image en base de donnée.

--- app/src/main/java/com/example/runtourist/base/AllMyCells.java
+++ app/src/main/java/com/example/runtourist/base/AllMyCells.java
@@ -1,29 +1,37 @@
 package com.example.runtourist.base;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
+import android.database.sqlite.*;
 
 import androidx.annotation.Nullable;
 
 public class AllMyCells extends SQLiteOpenHelper {
-    String TABLE_COMMENTS = "comments" ;
-    String COLUMN_ID = "_id";
-    String COLUMN_COMMENT ="comment";
-    String DATABASE_CREATE = "create table "+ TABLE_COMMENTS + "(" + COLUMN_ID + " integer primary key autoincrement, "+ COLUMN_COMMENT + " text not null ; ";
+    public static final String DATABASE_NAME = "AllCells.db";
+    public static final int    DATABASE_VERSION = 1;
+    public static final String TABLE_SERVEURS = "table_serveurs";
+    public static final String COL_ID = "ID";
+    public static final String COL_NOMLIEU = "NOMLIEU";
+
+    private static final String CREATE_BDD = "CREATE TABLE " + TABLE_SERVEURS + " ("
+            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COL_NOMLIEU + " TEXT NOT NULL );";
 
     public AllMyCells(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db)
+    {
+        // on crée la table
+        db.execSQL(CREATE_BDD);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int BDavant, int BDapres)
+    {
+        // au choix : on supprime la table puis on la recrée
+        db.execSQL("DROP TABLE " + TABLE_SERVEURS + ";");
+        onCreate(db);
     }
 }
