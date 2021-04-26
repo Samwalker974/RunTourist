94bdd9a12c48076f014826e34c8392a7bf7accde
MysterX02 <xavierl97490@hotmail.fr>
2021-04-26 09:06:54 +0400

    Base de donnée assez complet faudra faire des test mais imcomplet essaie d'implementation d'une image en base de donnée.

--- app/src/main/java/com/example/runtourist/base/BaseDonnee.java
+++ app/src/main/java/com/example/runtourist/base/BaseDonnee.java
@@ -1,6 +1,17 @@
 package com.example.runtourist.base;
 
+import android.content.*;
+import android.database.*;
+import android.database.sqlite.*;
+import android.util.*;
 public class BaseDonnee {
 
+    private SQLdatabase bdd = null;
+    private ServerSQLite serverSQLite = null;
+
+    public BaseDonnee(Context context) {
+        //creation d'une base et sa table
+        serverSQLite = new ServerSQLite(context);
+    }
 
 }
